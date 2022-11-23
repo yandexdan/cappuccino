b95b1db7453c06057900b4a642a8d694a5f05865
yandexdan <daniilkurdinkurdin@yandex.ru>
2022-11-23 13:10:53 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from random import randint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_file import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('git.ui', self)
         self.do_paint = False
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -25,7 +26,7 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw_round(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         x = randint(10, 250)
         y = randint(10, 390)
         qp.drawEllipse(x, x, y, y)
